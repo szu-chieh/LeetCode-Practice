2fce1cc3af9480fdbaeb6d5ba655d862901e2cb4
szu-chieh <szuchiehhuang0505@gmail.com>
2024-02-01 21:04:43 +0800

    update: 2095

---
+++ Blind75/Linked List/2095.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode deleteMiddle(ListNode head) {
+
+        // - 創建一個虛擬原點指向原本的head
+        // - 使用快指標和慢指標，其中快指標以慢指標的兩倍速度走訪linked list
+        // - 則當快指標走到末端停下時，slow的下一個節點就是要刪除的middle node
+        // - slow的next指向下下一個節點即為刪除
+
+        ListNode dummy = new ListNode(0, head);
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+
+        while (fast.next != null && fast.next.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        slow.next = slow.next.next;
+        return dummy.next;
+
+    }
+}
--- Blind75/readme.md
+++ Blind75/readme.md
@@ -1,2 +1,3 @@
 ### Linked List 
 (Easy) [206. Reverse Linked List](./Linked%20List/206_Reverse_Linked_List.java)
+(Medium) [2095. Delete the Middle Node of a Linked List](./Linked%20List/2095.java)
